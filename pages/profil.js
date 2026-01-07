e6953ff580302eb0aa7bcd198807da60264b941b
Anna <awoellnitz@web.de>
2026-01-07 17:06:12 +0100

    authfunctionpagelogin

--- pages/profil.js
+++ pages/profil.js
@@ -1,12 +1,21 @@
 import Navigation from "@/components/Navigation/navigation";
 import { StyledContainer, Subtitle, Title } from "@/components/styledPages";
+import { useSession } from "next-auth/react";
 
 export default function ProfilPage() {
-    return (
-        <StyledContainer>
-        <Title>myPAWS</Title>
-        <Subtitle>My Dogs/Cats/Pets/...</Subtitle>
-        <Navigation />
-        </StyledContainer>
-    )
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading...</p>;
+  if (!session) {
+    return <p>You must be logged in to view this page.</p>;
+  }
+
+  return (
+    <StyledContainer>
+      <Title>myPAWS</Title>
+      <Subtitle>My Dogs/Cats/Pets/...</Subtitle>
+      <h3>{session.user.name}`s Profile</h3>
+      <Navigation />
+    </StyledContainer>
+  );
 }
